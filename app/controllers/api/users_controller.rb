02e12e8852497eae899e5f9894c5c15800191a93
KUn Hee <kunhee617@gmail.com>
2020-09-22 11:12:26 -0400

    Auth front/back

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,19 +1,18 @@
 class Api::UsersController < ApplicationController
-    before_action :ensure_logged_in, only: [:show, :index]
-    def create
-        @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-        if @user.save
-            login(@user)
-            render "api/users/show"
-        else
-            render json: @user.errors.full_messages, status: 422
-        end
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
     end
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
