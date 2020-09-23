b68f60d5d4e960f23d4cc607a519dd76c04beb6f
KUn Hee <kunhee617@gmail.com>
2020-09-23 11:54:44 -0400

    fixed push

--- frontend/components/splash/splash_body/splashbodywords.jsx
+++ frontend/components/splash/splash_body/splashbodywords.jsx
@@ -8,6 +8,7 @@ class SplashBodyWords extends React.Component {
                 <h1 className='splashbodyheader'>Investing for Everyone</h1>
                 <h1 className='splashbodydescription'>Robinhood, a pioneer of commission-free investing, gives you
                 more ways to make your money work harder.</h1>
+                
                 <Link to="/signup">
                     <button className="splashbody-signup">Sign Up</button>
                 </Link>
