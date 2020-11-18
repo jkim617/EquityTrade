7fd42423ff4edb3d18ac6ab82eb51821b6f21fcf
KUn Hee <kunhee617@gmail.com>
2020-11-18 10:26:22 -0500

    swapped test keys

--- frontend/util/stocks_api_util.js
+++ frontend/util/stocks_api_util.js
@@ -4,16 +4,16 @@ const test_key = 'Tpk_744f03d1c887482bac12bddbadff8175'
 export const fetchCurrentPrice = (ticker) => {
     return $.ajax({
         method: 'GET',
-        // url: `https://cloud.iexapis.com/stable/stock/${ticker}/price?token=${prod_key}`
-        url: `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=${test_key}`
+        url: `https://cloud.iexapis.com/stable/stock/${ticker}/price?token=${prod_key}`
+        // url: `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=${test_key}`
     })
 }
 
 export const fetchIntradayPrices = (tickers) => {
     return $.ajax({
         method: 'GET',
-        // url: `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=intraday-prices&token=${prod_key}`
-        url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=intraday-prices&token=${test_key}`
+        url: `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=intraday-prices&token=${prod_key}`
+        // url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=intraday-prices&token=${test_key}`
     })
 }
 
@@ -21,8 +21,8 @@ export const fetchHistoricalPrices = (tickers, range) => {
 
     return $.ajax({
         method: 'GET',
-        // url: `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=chart&range=${range}&includeToday=true&token=${prod_key}`
-        url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=chart&range=${range}&includeToday=true&token=${test_key}`
+        url: `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=chart&range=${range}&includeToday=true&token=${prod_key}`
+        // url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${tickers}&types=chart&range=${range}&includeToday=true&token=${test_key}`
     })
 }
 
@@ -30,16 +30,16 @@ export const fetchCompany = (ticker) => {
 
     return $.ajax({
         method: 'GET',
-        // url: `https://cloud.iexapis.com/stable/stock/${ticker}/company?token=${prod_key}`
-        url: `https://sandbox.iexapis.com/stable/stock/${ticker}/company?token=${test_key}`
+        url: `https://cloud.iexapis.com/stable/stock/${ticker}/company?token=${prod_key}`
+        // url: `https://sandbox.iexapis.com/stable/stock/${ticker}/company?token=${test_key}`
     })
 }
 
 export const fetchSearchResults = (fragment) => {
     return $.ajax({
         method: 'GET',
-        // url: `https://cloud.iexapis.com/stable/search/${fragment}?token=${prod_key}`
-        url: `https://sandbox.iexapis.com/stable/search/${fragment}?token=${test_key}`
+        url: `https://cloud.iexapis.com/stable/search/${fragment}?token=${prod_key}`
+        // url: `https://sandbox.iexapis.com/stable/search/${fragment}?token=${test_key}`
     })
 }
 
