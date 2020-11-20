2edb799eb3e843df9b73542eeec26489cd8d49b7
KUn Hee <kunhee617@gmail.com>
2020-11-20 17:04:49 -0500

    fixed controller

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -2,14 +2,14 @@ class Api::StocksController < ApplicationController
     
     def index
         @stocks = current_user.stocks
-        render :index
+        # render :index
     end
 
     def create
         @stock = Stock.new(user_id: current_user, ticker: params[:ticker])
 
         if @stock.save
-            render :index
+            # render :index
         else
             render json: ["Invalid move"]
         end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :show]
-    resources :stocks, only: [:index, :show]
+    resources :stocks, only: [:index, :create]
     get '/stocks/:ticker', to: 'stocks#show'
     resource :session, only: [:create, :destroy, :show]
     resources :transactions, only: [:create, :index, :show]
