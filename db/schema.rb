31b2d5062ce3d568cfd0b8c2116498003a7e4407
KUn Hee <kunhee617@gmail.com>
2020-11-20 01:13:38 -0500

    added list backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_205250) do
+ActiveRecord::Schema.define(version: 2020_11_20_055646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker", null: false
+    t.integer "user_id", null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.string "ticker", null: false
